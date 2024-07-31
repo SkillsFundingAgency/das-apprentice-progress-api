d6fb8461636568beb93db397224b8171813d59bf
Ali Reid <ali.reid@live.co.uk>
2024-07-31 16:28:52 +0100

    Tidy

--- src/SFA.DAS.ApprenticeProgress.Application/Models/QueryResult.cs
+++ src/SFA.DAS.ApprenticeProgress.Application/Models/QueryResult.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace SFA.DAS.ApprenticeProgress.Application.Models
 {
     public abstract class QueryResult<T>
     {
-        //public List<T> Items { get; set; }
+        public List<T> Items { get; set; }
     }
 }
