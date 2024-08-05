c873020410188309b06f7322792d2f74bcdd94d6
Ali Reid <ali.reid@live.co.uk>
2024-08-05 11:09:46 +0100

    ksb progress history table

--- src/SFA.DAS.ApprenticeProgress.Database/Tables/KSBProgressStatusHistory.sql
+++ src/SFA.DAS.ApprenticeProgress.Database/Tables/KSBProgressStatusHistory.sql
@@ -1,6 +1,15 @@
-﻿CREATE TABLE [dbo].[KSBProgressStatusHistory] (
-    KSBProgressId BIGINT NOT NULL PRIMARY KEY,
-    Status INT NULL, 
-    StatusTime DATETIME2 NULL,
-);
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[KSBProgressStatusHistory](
+    [KSBProgressId] [bigint] NOT NULL,
+    [Status] [int] NULL,
+    [StatusTime] [datetime2](7) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[KSBProgressStatusHistory] ADD PRIMARY KEY CLUSTERED 
+(
+    [KSBProgressId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
