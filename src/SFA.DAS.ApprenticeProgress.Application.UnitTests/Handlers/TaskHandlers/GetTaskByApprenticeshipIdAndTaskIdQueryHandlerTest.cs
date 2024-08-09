9c8effc272b3b00c5fdc5756936bf56d1c5bd44b
Ali Reid <ali.reid@live.co.uk>
2024-08-09 10:53:14 +0100

    tests

--- src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/TaskHandlers/GetTaskByApprenticeshipIdAndTaskIdQueryHandlerTest.cs
+++ src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/TaskHandlers/GetTaskByApprenticeshipIdAndTaskIdQueryHandlerTest.cs
@@ -22,16 +22,26 @@ namespace SFA.DAS.ApprenticeProgress.Application.UnitTests.DataFixture
 
             var getTaskByApprenticeshipIdAndTaskIdQueryHandler = new GetTaskByApprenticeshipIdAndTaskIdQueryHandler(DbContext);
 
-            var command = new GetTaskByApprenticeshipIdAndTaskIdQuery();
-          
+            var command = new GetTaskByApprenticeshipIdAndTaskIdQuery
+            {
+                TaskId = 1,
+                ApprenticeshipId = new Guid("fd0daf58-af19-440d-b52f-7e1d47267d3b")
+            };
+
             var result = await getTaskByApprenticeshipIdAndTaskIdQueryHandler.Handle(command, CancellationToken.None);
             Assert.That(result, Is.Not.Null);
         }
 
         private async System.Threading.Tasks.Task PopulateDbContext()
         {
-            var ksbprogress = _fixture.CreateMany<Domain.Entities.Task>().ToArray();
-            await DbContext.Task.AddRangeAsync(ksbprogress);
+            var tasks = _fixture.CreateMany<Domain.Entities.Task>().ToArray();
+
+            tasks[0].DueDate = new DateTime(2025, 1, 1);
+            tasks[0].ApprenticeshipId = new Guid("fd0daf58-af19-440d-b52f-7e1d47267d3b");
+            tasks[0].Status = Task.TaskStatus.Todo;
+            tasks[0].TaskId = 1;
+
+            await DbContext.Task.AddRangeAsync(tasks);
             await DbContext.SaveChangesAsync();
         }
 
--- src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/TaskHandlers/GetTasksByApprenticeshipIdQueryHandlerTest.cs
+++ src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/TaskHandlers/GetTasksByApprenticeshipIdQueryHandlerTest.cs
@@ -16,7 +16,24 @@ namespace SFA.DAS.ApprenticeProgress.Application.UnitTests.DataFixture
         private readonly Fixture _fixture = new();
 
         [Test, MoqAutoData]
-        public async System.Threading.Tasks.Task GetTasksByApprenticeshipIdQueryHandler_test_status1()
+        public async System.Threading.Tasks.Task GetTasksByApprenticeshipIdQueryHandler_test_status_todo()
+        {
+            await PopulateDbContext();
+
+            var getTasksByApprenticeshipIdQueryHandler = new GetTasksByApprenticeshipIdQueryHandler(DbContext);
+
+            var command = new GetTasksByApprenticeshipIdQuery();
+            command.Status = 0;
+            command.ApprenticeshipId = new Guid("fd0daf58-af19-440d-b52f-7e1d47267d3b");
+            command.FromDate = new DateTime(2020, 1, 1);
+            command.ToDate = new DateTime(2030, 1, 1);
+
+            var result = await getTasksByApprenticeshipIdQueryHandler.Handle(command, CancellationToken.None);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test, MoqAutoData]
+        public async System.Threading.Tasks.Task GetTasksByApprenticeshipIdQueryHandler_test_status_done()
         {
             await PopulateDbContext();
 
@@ -24,30 +41,25 @@ namespace SFA.DAS.ApprenticeProgress.Application.UnitTests.DataFixture
 
             var command = new GetTasksByApprenticeshipIdQuery();
             command.Status = 1;
-
-            var result = await getTasksByApprenticeshipIdQueryHandler.Handle(command, CancellationToken.None);
-            Assert.That(result, Is.Not.Null);
-        }
-
-
-        [Test, MoqAutoData]
-        public async System.Threading.Tasks.Task GetTasksByApprenticeshipIdQueryHandler_test_status2()
-        {
-            await PopulateDbContext();
-
-            var getTasksByApprenticeshipIdQueryHandler = new GetTasksByApprenticeshipIdQueryHandler(DbContext);
-
-            var command = new GetTasksByApprenticeshipIdQuery();
-            command.Status = 2;
-
+            command.ApprenticeshipId = new Guid("fd0daf58-af19-440d-b52f-7e1d47267d3b");
+            command.FromDate = new DateTime(2020, 1, 1);
+            command.ToDate = new DateTime(2030, 1, 1);
             var result = await getTasksByApprenticeshipIdQueryHandler.Handle(command, CancellationToken.None);
             Assert.That(result, Is.Not.Null);
         }
 
         private async System.Threading.Tasks.Task PopulateDbContext()
         {
-            var ksbprogress = _fixture.CreateMany<Domain.Entities.Task>().ToArray();
-            await DbContext.Task.AddRangeAsync(ksbprogress);
+            var tasks = _fixture.CreateMany<Domain.Entities.Task>().ToArray();
+            tasks[0].DueDate = new DateTime(2025, 1, 1);
+            tasks[0].ApprenticeshipId = new Guid("fd0daf58-af19-440d-b52f-7e1d47267d3b");
+            tasks[0].Status = Task.TaskStatus.Todo;
+
+            tasks[1].DueDate = new DateTime(2025, 1, 1);
+            tasks[1].ApprenticeshipId = new Guid("fd0daf58-af19-440d-b52f-7e1d47267d3b");
+            tasks[1].Status = Task.TaskStatus.Done;
+
+            await DbContext.Task.AddRangeAsync(tasks);
             await DbContext.SaveChangesAsync();
         }
 
