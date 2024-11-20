5d5d3c78d0e19537fa3fb50d60953a309de871b0
CathyGroom <147840579+CathyGroom@users.noreply.github.com>
2024-11-20 11:16:11 +0000

    make apprenticeaccountidentifier not nullable

--- src/SFA.DAS.PushNotifications.Messages/Commands/SendPushNotificationCommand.cs
+++ src/SFA.DAS.PushNotifications.Messages/Commands/SendPushNotificationCommand.cs
@@ -6,7 +6,7 @@ namespace SFA.DAS.PushNotifications.Messages.Commands
     [ExcludeFromCodeCoverage]
     public class SendPushNotificationCommand
     {
-        public Guid? ApprenticeAccountIdentifier { get; set; } = null!;
+        public Guid ApprenticeAccountIdentifier { get; set; }
         public string Title { get; set; } = null!;
         public string Body { get; set; } = null!;
     }
