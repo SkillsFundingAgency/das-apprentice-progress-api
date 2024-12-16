ce0079679829c1429faba28acd2e743411378409
Ali Reid <ali.reid@live.co.uk>
2024-12-16 13:21:37 +0000

    Pen test work WIP

--- src/SFA.DAS.ApprenticeProgress.Api/AppStart/ExceptionMiddlewareExtensions.cs
+++ src/SFA.DAS.ApprenticeProgress.Api/AppStart/ExceptionMiddlewareExtensions.cs
@@ -25,6 +25,19 @@ namespace SFA.DAS.ApprenticeProgress.Api.AppStart
                     return System.Threading.Tasks.Task.CompletedTask;
                 });
             });
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+                context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
+                context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; img-src 'self' *.googletagmanager.com *.google-analytics.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googletagmanager.com *.google-analytics.com *.googleapis.com; font-src 'self' data:; connect-src 'self' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com;");
+
+                await next();
+            });
+
         }
     }
 }
