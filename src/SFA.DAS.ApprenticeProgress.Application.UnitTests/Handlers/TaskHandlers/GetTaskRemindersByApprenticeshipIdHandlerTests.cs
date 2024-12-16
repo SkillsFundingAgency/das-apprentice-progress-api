aeb2e0ea9e3f60374a7e0b28c9dc03bac5725e34
CathyGroom <147840579+CathyGroom@users.noreply.github.com>
2024-12-16 14:47:55 +0000

    AAD-729- add test

--- src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/TaskHandlers/GetTaskRemindersByApprenticeshipIdHandlerTests.cs
+++ src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/TaskHandlers/GetTaskRemindersByApprenticeshipIdHandlerTests.cs
@@ -28,6 +28,28 @@ namespace SFA.DAS.ApprenticeProgress.Application.UnitTests.DataFixture
             var result = await getTaskRemindersByApprenticeshipIdQueryHandler.Handle(request, CancellationToken.None);
             Assert.That(result, Is.Not.Null);
         }
+
+        [Test, MoqAutoData]
+        public async System.Threading.Tasks.Task GetTaskRemindersByApprenticeshipIdQueryHandler_Validate_Reminder()
+        {
+            await PopulateDbContext();
+
+            var getTaskRemindersByApprenticeshipIdQueryHandler = new GetTaskRemindersByApprenticeshipIdHandler(DbContext);
+
+            var request = new GetTaskRemindersByApprenticeshipIdQuery
+            {
+                ApprenticeshipId = 1
+            };
+
+            var result = await getTaskRemindersByApprenticeshipIdQueryHandler.Handle(request, CancellationToken.None);
+            Assert.That(result.TaskReminders.FirstOrDefault().TaskId, Is.EqualTo(1));
+            Assert.That(result.TaskReminders.FirstOrDefault().ApprenticeshipId, Is.EqualTo(1));
+            Assert.That(result.TaskReminders.FirstOrDefault().ApprenticeAccountId, Is.Not.Null);
+            Assert.That(result.TaskReminders.FirstOrDefault().DueDate, Is.EqualTo(new DateTime(2025, 1, 1)));
+            Assert.That(result.TaskReminders.FirstOrDefault().Title, Is.Not.Null);
+            Assert.That(result.TaskReminders.FirstOrDefault().ReminderValue, Is.EqualTo(5));
+        }
+
         private async System.Threading.Tasks.Task PopulateDbContext()
         {
             var tasks = _fixture.CreateMany<Domain.Entities.Task>().ToArray();
@@ -40,9 +62,10 @@ namespace SFA.DAS.ApprenticeProgress.Application.UnitTests.DataFixture
             await DbContext.SaveChangesAsync();
 
             var taskReminders = _fixture.CreateMany<Domain.Entities.TaskReminder>().ToArray();
-            taskReminders[0].Status = ReminderStatus.NotSent;
+            taskReminders[0].Status = ReminderStatus.Sent;
             taskReminders[0].TaskId = 1;
-            taskReminders[1].Status = ReminderStatus.Sent;
+            taskReminders[0].ReminderValue = 5;
+            taskReminders[1].Status = ReminderStatus.NotSent;
 
             await DbContext.TaskReminder.AddRangeAsync(taskReminders);
             await DbContext.SaveChangesAsync();
