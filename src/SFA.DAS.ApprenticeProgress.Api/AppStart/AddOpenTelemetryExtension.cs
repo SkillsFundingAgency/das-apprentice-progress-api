317afdddf98857d15c1104cc3077a7718e1bdd1a
tdavande <113100339+tdavande@users.noreply.github.com>
2024-12-17 10:47:59 +0000

    exclude opentelemetry config code for code coverage

--- src/SFA.DAS.ApprenticeProgress.Api/AppStart/AddOpenTelemetryExtension.cs
+++ src/SFA.DAS.ApprenticeProgress.Api/AppStart/AddOpenTelemetryExtension.cs
@@ -1,8 +1,10 @@
-﻿using Azure.Monitor.OpenTelemetry.AspNetCore;
+﻿using System.Diagnostics.CodeAnalysis;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SFA.DAS.FindAnApprenticeship.Api.AppStart
 {
+    [ExcludeFromCodeCoverage]
     public static class AddOpenTelemetryExtensions
     {
         public static void AddOpenTelemetryRegistration(this IServiceCollection services, string APPLICATIONINSIGHTS_CONNECTION_STRING)
