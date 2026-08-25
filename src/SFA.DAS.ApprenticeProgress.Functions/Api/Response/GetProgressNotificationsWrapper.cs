795acb1b35b6acb95436d8e675c47036b8053000
NWake-CGI <nick.wake@cgi.com>
2026-08-25 12:35:28 +0100

    Apprentice-Progress-Notifications-Functions

---
+++ src/SFA.DAS.ApprenticeProgress.Functions/Api/Response/GetProgressNotificationsWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeProgress.Functions.Api.Response
+{
+    public class GetProgressNotificationsWrapper
+    {
+        public List<Domain.Entities.ApprenticeshipProgressNotification> Notifications { get; set; }
+    }
+}
