bb33573f9c8f587334c0f5787eb00f29c411b0e7
NWake-CGI <nick.wake@cgi.com>
2026-08-25 12:30:13 +0100

    Apprenticeship-Progress-Notifications-API

---
+++ src/SFA.DAS.ApprenticeProgress.Api/Controllers/NotificationController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeProgress.Application.Notifications.Commands.AddApprenticeshipProgress;
+using SFA.DAS.ApprenticeProgress.Application.Notifications.Commands.UpdateProgressNotificationStatus;
+using SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetApprenticeshipProgressNotifications;
+using SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetProgressNotificationsById;
+using SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetProgressNotificationsToCheck;
+
+namespace SFA.DAS.ApprenticeProgress.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("notifications/")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NotificationController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPost("apprenticeship-progress/{apprenticeIdentifier}/")]
+        public async Task<IActionResult> CreateApprenticeshipProgress(Guid apprenticeIdentifier)
+        {
+            var result = await _mediator.Send(new AddApprenticeshipProgressCommand
+            {
+                ApprenticeIdentifier = apprenticeIdentifier
+            });
+
+            return Ok(result);
+        }
+
+        [HttpGet("apprenticeship-progress/{apprenticeIdentifier}")]
+        public async Task<IActionResult> GetApprenticeProgressByApprenticeId(Guid apprenticeIdentifier)
+        {
+            var result = await _mediator.Send(new GetApprenticeshipProgressNotificationsByIdQuery
+            {
+                ApprenticeIdentifier = apprenticeIdentifier
+            });
+
+            return Ok(result);
+        }
+
+        [HttpGet("progress-notification/{notificationId}")]
+        public async Task<IActionResult> GetProgressNotificationById(Guid notificationId)
+        {
+            var result = await _mediator.Send(new GetProgressNotificationsByIdQuery
+            {
+                NotificationId = notificationId
+            });
+
+            return Ok(result);
+        }
+
+        [HttpGet("progress-notification-to-check")]
+        public async Task<IActionResult> GetProgressNotificationsToCheck()
+        {
+            var result = await _mediator.Send(new GetProgressNotificationsToCheckQuery());
+            return Ok(result);
+        }
+
+        [HttpPatch("updateProgressNotificationStatus/{notificationId}/{apprenticeProgressId}")]
+        public async Task<IActionResult> UpdateProgressNotificationStatus(Guid notificationId, long apprenticeProgressId)
+        {
+            var result = await _mediator.Send(new UpdateProgressNotificationStatusCommand
+            {
+                NotificationId = notificationId,
+                ApprenticeProgressId = apprenticeProgressId            
+            });
+
+            return Ok();
+        }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeProgress.Application/Notifications/Queries/GetProgressNotificationsToCheck/GetProgressNotificationsToCheckQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.ApprenticeProgress.Data;
+
+namespace SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetProgressNotificationsToCheck
+{
+    public class GetProgressNotificationsToCheckQueryHandler : IRequestHandler<GetProgressNotificationsToCheckQuery, GetProgressNotificationsToCheckResult>
+    {
+
+        private readonly ApprenticeProgressDataContext _ApprenticeProgressDataContext;
+
+        public GetProgressNotificationsToCheckQueryHandler(ApprenticeProgressDataContext apprenticeProgressDataContext)
+        {
+            _ApprenticeProgressDataContext = apprenticeProgressDataContext;
+        }
+
+        public async Task<GetProgressNotificationsToCheckResult> Handle(GetProgressNotificationsToCheckQuery request, CancellationToken cancellationToken)
+        {
+            var notificationsToCheck = await _ApprenticeProgressDataContext.ApprenticeshipProgressNotification
+                .Include(x => x.ApprenticeshipProgress)
+                .Include(x => x.ProgressNotification)
+                .Where(x => x.IsEnabled == true)
+                .ToListAsync();
+
+            var result = new GetProgressNotificationsToCheckResult
+            {
+                Notifications = notificationsToCheck
+            };
+
+            return result;
+        }
+    }
+}
