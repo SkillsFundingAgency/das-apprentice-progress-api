50ee11c69b5834f947341495ba687bbe524bc6cd
NWake-CGI <nick.wake@cgi.com>
2026-08-25 13:39:07 +0100

    updat tests

---
+++ src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/NotificationHandlers/AddApprenticeshipProgressCommandHandlerTest.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using AutoFixture;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeProgress.Application.Notifications.Commands.AddApprenticeshipProgress;
+using SFA.DAS.ApprenticeProgress.Application.UnitTests.DataFixture;
+using SFA.DAS.ApprenticeProgress.Domain.Entities;
+using Task = System.Threading.Tasks.Task;
+
+namespace SFA.DAS.ApprenticeProgress.Application.UnitTests.Handlers.NotificationHandlers
+{
+    public class AddApprenticeshipProgressCommandHandlerTest : ApprenticeProgressDbContextFixture
+    {
+        private readonly Fixture _fixture = new();
+
+        [Test]
+        public async Task Handle_CreatesApprenticeshipProgress()
+        {
+            // Arrange
+            var apprenticeIdentifier = Guid.NewGuid();
+
+            var command = _fixture.Build<AddApprenticeshipProgressCommand>()
+                .With(x => x.ApprenticeIdentifier, apprenticeIdentifier)
+                .Create();
+
+            var handler = new AddApprenticeshipProgressCommandHandler(DbContext);
+
+            var beforeHandle = DateTime.Now;
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            var afterHandle = DateTime.Now;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+
+            var savedProgress = await DbContext.ApprenticeshipProgress
+                .SingleOrDefaultAsync(x =>
+                    x.ApprenticeAccountId == apprenticeIdentifier);
+
+            Assert.That(savedProgress, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(
+                    savedProgress.ApprenticeAccountId,
+                    Is.EqualTo(apprenticeIdentifier));
+
+                Assert.That(
+                    savedProgress.IsEnabled,
+                    Is.True);
+
+                Assert.That(
+                    savedProgress.FirstLoggedIn,
+                    Is.InRange(beforeHandle, afterHandle));
+            });
+        }
+
+        [Test]
+        public async Task Handle_WhenEnabledNotificationsExist_CreatesProgressNotifications()
+        {
+            // Arrange
+            var notification1 = _fixture.Build<ProgressNotification>()
+                .With(x => x.Id, (Guid?)null)
+                .With(x => x.IsEnabled, true)
+                .Create();
+
+            var notification2 = _fixture.Build<ProgressNotification>()
+                .With(x => x.Id, (Guid?)null)
+                .With(x => x.IsEnabled, true)
+                .Create();
+
+            await DbContext.ProgressNotification.AddRangeAsync(
+                notification1,
+                notification2);
+
+            await DbContext.SaveChangesAsync();
+
+            var command = _fixture.Build<AddApprenticeshipProgressCommand>()
+                .With(x => x.ApprenticeIdentifier, Guid.NewGuid())
+                .Create();
+
+            var handler = new AddApprenticeshipProgressCommandHandler(DbContext);
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+
+            var savedProgress = await DbContext.ApprenticeshipProgress
+                .SingleAsync(x =>
+                    x.ApprenticeAccountId == command.ApprenticeIdentifier);
+
+            var progressNotifications =
+                await DbContext.ApprenticeshipProgressNotification
+                    .Where(x => x.ApprenticeProgressId == savedProgress.Id)
+                    .ToListAsync();
+
+            Assert.That(progressNotifications, Has.Count.EqualTo(2));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(
+                    progressNotifications.Any(x =>
+                        x.NotificationId == notification1.Id),
+                    Is.True);
+
+                Assert.That(
+                    progressNotifications.Any(x =>
+                        x.NotificationId == notification2.Id),
+                    Is.True);
+
+                Assert.That(
+                    progressNotifications.All(x => x.IsEnabled),
+                    Is.True);
+            });
+        }
+
+        [Test]
+        public async Task Handle_WhenDisabledNotificationExists_DoesNotCreateProgressNotification()
+        {
+            // Arrange
+            var enabledNotification = _fixture.Build<ProgressNotification>()
+                .With(x => x.Id, (Guid?)null)
+                .With(x => x.IsEnabled, true)
+                .Create();
+
+            var disabledNotification = _fixture.Build<ProgressNotification>()
+                .With(x => x.Id, (Guid?)null)
+                .With(x => x.IsEnabled, false)
+                .Create();
+
+            await DbContext.ProgressNotification.AddRangeAsync(
+                enabledNotification,
+                disabledNotification);
+
+            await DbContext.SaveChangesAsync();
+
+            var command = _fixture.Build<AddApprenticeshipProgressCommand>()
+                .With(x => x.ApprenticeIdentifier, Guid.NewGuid())
+                .Create();
+
+            var handler = new AddApprenticeshipProgressCommandHandler(DbContext);
+
+            // Act
+            await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            var savedProgress = await DbContext.ApprenticeshipProgress
+                .SingleAsync(x =>
+                    x.ApprenticeAccountId == command.ApprenticeIdentifier);
+
+            var progressNotifications =
+                await DbContext.ApprenticeshipProgressNotification
+                    .Where(x => x.ApprenticeProgressId == savedProgress.Id)
+                    .ToListAsync();
+
+            Assert.That(progressNotifications, Has.Count.EqualTo(1));
+
+            var progressNotification = progressNotifications.Single();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(
+                    progressNotification.NotificationId,
+                    Is.EqualTo(enabledNotification.Id));
+
+                Assert.That(
+                    progressNotification.NotificationId,
+                    Is.Not.EqualTo(disabledNotification.Id));
+
+                Assert.That(
+                    progressNotification.IsEnabled,
+                    Is.True);
+            });
+        }
+
+        [Test]
+        public async Task Handle_WhenNoEnabledNotificationsExist_CreatesProgressWithoutNotifications()
+        {
+            // Arrange
+            var disabledNotification = _fixture.Build<ProgressNotification>()
+                .With(x => x.Id, (Guid?)null)
+                .With(x => x.IsEnabled, false)
+                .Create();
+
+            await DbContext.ProgressNotification.AddAsync(disabledNotification);
+            await DbContext.SaveChangesAsync();
+
+            var command = _fixture.Build<AddApprenticeshipProgressCommand>()
+                .With(x => x.ApprenticeIdentifier, Guid.NewGuid())
+                .Create();
+
+            var handler = new AddApprenticeshipProgressCommandHandler(DbContext);
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+
+            var savedProgress = await DbContext.ApprenticeshipProgress
+                .SingleOrDefaultAsync(x =>
+                    x.ApprenticeAccountId == command.ApprenticeIdentifier);
+
+            Assert.That(savedProgress, Is.Not.Null);
+
+            var progressNotifications =
+                await DbContext.ApprenticeshipProgressNotification
+                    .Where(x => x.ApprenticeProgressId == savedProgress.Id)
+                    .ToListAsync();
+
+            Assert.That(progressNotifications, Is.Empty);
+        }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeProgress.Application.UnitTests/Handlers/NotificationHandlers/UpdateProgressNotificationStatusCommandHandlerTest.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Threading;
+using AutoFixture;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeProgress.Application.Notifications.Commands.UpdateProgressNotificationStatus;
+using SFA.DAS.ApprenticeProgress.Application.UnitTests.DataFixture;
+using SFA.DAS.ApprenticeProgress.Domain.Entities;
+using Task = System.Threading.Tasks.Task;
+
+namespace SFA.DAS.ApprenticeProgress.Application.UnitTests.Handlers.NotificationHandlers
+{
+    public class UpdateProgressNotificationStatusCommandHandlerTest
+        : ApprenticeProgressDbContextFixture
+    {
+        private readonly Fixture _fixture = new();
+
+        [Test]
+        public async Task Handle_WhenNotificationExists_DisablesNotification()
+        {
+            // Arrange
+            var notification = _fixture.Build<ApprenticeshipProgressNotification>()
+                .With(x => x.IsEnabled, true)
+                .Create();
+
+            await DbContext.ApprenticeshipProgressNotification.AddAsync(notification);
+            await DbContext.SaveChangesAsync();
+
+            var command = _fixture.Build<UpdateProgressNotificationStatusCommand>()
+                .With(x => x.NotificationId, notification.NotificationId)
+                .With(x => x.ApprenticeProgressId, notification.ApprenticeProgressId)
+                .Create();
+
+            var handler = new UpdateProgressNotificationStatusCommandHandler(DbContext);
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+
+            var updatedNotification = await DbContext.ApprenticeshipProgressNotification
+                .SingleAsync(x =>
+                    x.NotificationId == command.NotificationId &&
+                    x.ApprenticeProgressId == command.ApprenticeProgressId);
+
+            Assert.That(updatedNotification.IsEnabled, Is.False);
+        }
+
+        [Test]
+        public async Task Handle_WhenNotificationDoesNotExist_ReturnsUnitValue()
+        {
+            // Arrange
+            var command = _fixture.Build<UpdateProgressNotificationStatusCommand>()
+                .With(x => x.NotificationId, Guid.NewGuid())
+                .With(x => x.ApprenticeProgressId, 999999)
+                .Create();
+
+            var handler = new UpdateProgressNotificationStatusCommandHandler(DbContext);
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+        }
+
+        [Test]
+        public async Task Handle_WhenNotificationIdDoesNotMatch_DoesNotDisableNotification()
+        {
+            // Arrange
+            var notification = _fixture.Build<ApprenticeshipProgressNotification>()
+                .With(x => x.IsEnabled, true)
+                .Create();
+
+            await DbContext.ApprenticeshipProgressNotification.AddAsync(notification);
+            await DbContext.SaveChangesAsync();
+
+            var command = _fixture.Build<UpdateProgressNotificationStatusCommand>()
+                .With(x => x.NotificationId, Guid.NewGuid())
+                .With(x => x.ApprenticeProgressId, notification.ApprenticeProgressId)
+                .Create();
+
+            var handler = new UpdateProgressNotificationStatusCommandHandler(DbContext);
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+
+            var unchangedNotification = await DbContext.ApprenticeshipProgressNotification
+                .SingleAsync(x =>
+                    x.NotificationId == notification.NotificationId &&
+                    x.ApprenticeProgressId == notification.ApprenticeProgressId);
+
+            Assert.That(unchangedNotification.IsEnabled, Is.True);
+        }
+
+        [Test]
+        public async Task Handle_WhenApprenticeProgressIdDoesNotMatch_DoesNotDisableNotification()
+        {
+            // Arrange
+            var notification = _fixture.Build<ApprenticeshipProgressNotification>()
+                .With(x => x.IsEnabled, true)
+                .Create();
+
+            await DbContext.ApprenticeshipProgressNotification.AddAsync(notification);
+            await DbContext.SaveChangesAsync();
+
+            var command = _fixture.Build<UpdateProgressNotificationStatusCommand>()
+                .With(x => x.NotificationId, notification.NotificationId)
+                .With(x => x.ApprenticeProgressId, notification.ApprenticeProgressId + 1)
+                .Create();
+
+            var handler = new UpdateProgressNotificationStatusCommandHandler(DbContext);
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+
+            var unchangedNotification = await DbContext.ApprenticeshipProgressNotification
+                .SingleAsync(x =>
+                    x.NotificationId == notification.NotificationId &&
+                    x.ApprenticeProgressId == notification.ApprenticeProgressId);
+
+            Assert.That(unchangedNotification.IsEnabled, Is.True);
+        }
+    }
+}
