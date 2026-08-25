93d6a3db90fa6e22d141954d9fbfd7dbe28e33a5
NWake-CGI <nick.wake@cgi.com>
2026-08-25 12:23:16 +0100

    Apprentice-Progress-Notifications-Database

---
+++ src/SFA.DAS.ApprenticeProgress.Database/Tables/ApprenticeshipProgress.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ApprenticeshipProgress]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+    [ApprenticeAccountId] UNIQUEIDENTIFIER NOT NULL,
+    [ApprenticeshipId] NCHAR(10) NULL,
+    [FirstLoggedIn] DATETIME2 NOT NULL,
+    [LastLoggedIn] DATETIME2 NULL,
+    [StartDate] DATETIME2 NULL,
+    [PlannedEndDate] DATETIME2 NULL,
+    [IsEnabled] BIT NOT NULL
+);
+GO
---
+++ src/SFA.DAS.ApprenticeProgress.Domain/Entities/ProgressNotification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SFA.DAS.ApprenticeProgress.Domain.Entities
+{
+    public class ProgressNotification
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string NotificationId { get; set; }
+        public string NotificationScope { get; set; }
+        public bool IsEnabled { get; set; }
+        public ActivationPoint ActivationPoint { get; set; }
+        public string Delay { get; set; }
+        public DelayUnit DelayUnit { get; set; }
+    }
+
+    public enum ActivationPoint 
+    {
+        FromStart = 1,
+        FromEnd = 2,
+        Immediate = 3
+    }
+
+    public enum DelayUnit
+    {
+        Day = 1,
+        Week = 2,
+        Month = 3
+    }
+}
