42ea5c568935abc871e7abd506fef1af5654ae61
NWake-CGI <nick.wake@cgi.com>
2026-08-25 12:30:13 +0100

    Apprenticeship-Progress-Notifications-API

---
+++ src/SFA.DAS.ApprenticeProgress.Application/Notifications/Queries/GetApprenticeshipProgressNotifications/GetApprenticeshipProgressNotificationsByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetApprenticeshipProgressNotifications
+{
+    public class GetApprenticeshipProgressNotificationsByIdQuery : IRequest<GetApprenticeshipProgressNotificationsByIdResult>
+    {
+        public Guid ApprenticeIdentifier { get; set; }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeProgress.Application/Notifications/Queries/GetProgressNotificationsById/GetProgressNotificationsByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.ApprenticeProgress.Data;
+
+namespace SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetProgressNotificationsById
+{
+    public class GetProgressNotificationsByIdQueryHandler : IRequestHandler<GetProgressNotificationsByIdQuery, GetProgressNotificationsByIdResult>
+    {
+        private readonly ApprenticeProgressDataContext _ApprenticeProgressDataContext;
+
+        public GetProgressNotificationsByIdQueryHandler(ApprenticeProgressDataContext apprenticeProgressDataContext)
+        {
+            _ApprenticeProgressDataContext = apprenticeProgressDataContext;
+        }
+
+        public async Task<GetProgressNotificationsByIdResult> Handle(GetProgressNotificationsByIdQuery request, CancellationToken cancellationToken)
+        {
+            var notification = await _ApprenticeProgressDataContext.ProgressNotification
+                .Where(noti => noti.Id == request.NotificationId)
+                .FirstOrDefaultAsync();
+
+            var result = new GetProgressNotificationsByIdResult
+            {
+                ProgressNotification = notification
+            };
+
+            return result;
+        }
+
+    }
+}
