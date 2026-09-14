0e6f63872ab983dda0cd88b7e4f36d144913ab7b
NWake-CGI <nick.wake@cgi.com>
2026-09-14 15:45:55 +0100

    update program to exclude

--- src/SFA.DAS.ApprenticeProgress.Functions/Program.cs
+++ src/SFA.DAS.ApprenticeProgress.Functions/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,28 +9,40 @@ using SFA.DAS.ApprenticeProgress.Functions.Configuration;
 using SFA.DAS.ApprenticeProgress.Functions.Extensions;
 using SFA.DAS.ApprenticeProgress.Functions.HttpClientConfiguration;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureAppConfiguration(
-        builder =>
-        {
-            builder.AddConfiguration();            
-        })
-    .ConfigureServices((context, s) =>
+namespace SFA.DAS.ApprenticeProgress.Functions;
+
+[ExcludeFromCodeCoverage]
+public static class Program
+{
+    public static async Task Main(string[] args)
     {
-        s
-            .AddOptions()
-            .Configure<ApplicationConfiguration>(context.Configuration.GetSection(nameof(ApplicationConfiguration)))            
-            .ConfigureHttpClients(context.Configuration)
-            .AddApplicationRegistrations()            
-            .AddNServiceBus(context.Configuration);
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddConfiguration();
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services
+                    .AddOptions()
+                    .Configure<ApplicationConfiguration>(
+                        context.Configuration.GetSection(
+                            nameof(ApplicationConfiguration)))
+                    .ConfigureHttpClients(context.Configuration)
+                    .AddApplicationRegistrations()
+                    .AddNServiceBus(context.Configuration);
 
-        s.AddDbContext<ApprenticeProgressDataContext>(options =>
-        options.UseSqlServer(context.Configuration.GetConnectionString("SqlConnectionString")));
+                services.AddDbContext<ApprenticeProgressDataContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString(
+                            "SqlConnectionString")));
 
-        s.AddScoped<IApprenticeProgressDataContext>(sp =>
-        sp.GetRequiredService<ApprenticeProgressDataContext>());
-    })
-    .Build();
+                services.AddScoped<IApprenticeProgressDataContext>(sp =>
+                    sp.GetRequiredService<ApprenticeProgressDataContext>());
+            })
+            .Build();
 
-await host.RunAsync();
+        await host.RunAsync();
+    }
+}
