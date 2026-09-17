76e9c8a66e404b545e8cbd0286f878d0440611e7
NWake-CGI <nick.wake@cgi.com>
2026-09-17 10:32:04 +0100

    update to notifications to check

--- src/SFA.DAS.ApprenticeProgress.Application/Notifications/Queries/GetProgressNotificationsToCheck/GetProgressNotificationsToCheckQueryHandler.cs
+++ src/SFA.DAS.ApprenticeProgress.Application/Notifications/Queries/GetProgressNotificationsToCheck/GetProgressNotificationsToCheckQueryHandler.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SFA.DAS.ApprenticeProgress.Data;
+using SFA.DAS.ApprenticeProgress.Domain.Entities;
 
 namespace SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetProgressNotificationsToCheck
 {
@@ -25,9 +27,42 @@ namespace SFA.DAS.ApprenticeProgress.Application.Notifications.Queries.GetProgre
                 .Where(x => x.IsEnabled == true)
                 .ToListAsync(cancellationToken);
 
+            var now = DateTime.UtcNow;
+
+            var notificationsDue = notificationsToCheck
+                .Where(x =>
+                {
+                    var notification = x.ProgressNotification;
+                    var progress = x.ApprenticeshipProgress;
+
+                    DateTime? activationDate = notification.ActivationPoint switch
+                    {
+                        ActivationPoint.FromStart => progress.FirstLoggedIn,
+                        ActivationPoint.FromEnd => progress.PlannedEndDate,
+                        _ => null
+                    };
+
+                    if (activationDate == null)
+                        return false;
+
+                    var notificationDate = notification.DelayUnit switch
+                    {
+                        DelayUnit.Day => activationDate.Value.AddDays(int.Parse(notification.Delay)),
+                        DelayUnit.Week => activationDate.Value.AddDays(int.Parse(notification.Delay) * 7),
+                        DelayUnit.Month => activationDate.Value.AddMonths(int.Parse(notification.Delay)),
+                        _ => throw new ArgumentOutOfRangeException(
+                            nameof(notification.DelayUnit),
+                            notification.DelayUnit,
+                            null)
+                    };
+
+                    return notificationDate.Date <= now.Date;
+                })
+                .ToList();
+
             var result = new GetProgressNotificationsToCheckResult
             {
-                Notifications = notificationsToCheck
+                Notifications = notificationsDue
             };
 
             return result;
