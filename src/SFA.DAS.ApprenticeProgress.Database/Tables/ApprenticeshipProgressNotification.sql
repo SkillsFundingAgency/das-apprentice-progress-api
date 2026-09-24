4acccd7454bfaa43b25df72048b1d2565dc2ce47
NWake-CGI <nick.wake@cgi.com>
2026-09-24 17:27:25 +0100

    update typo

--- src/SFA.DAS.ApprenticeProgress.Database/Tables/ApprenticeshipProgressNotification.sql
+++ src/SFA.DAS.ApprenticeProgress.Database/Tables/ApprenticeshipProgressNotification.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[ApprenticeshipProgressNotification]
 (
-    [Id] INT NOT NULL PRIMARY KEY,
-    [ApprenitceProgressId] INT NOT NULL,
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [ApprenticeProgressId] INT NOT NULL,
     [NotificationId] UNIQUEIDENTIFIER NOT NULL,
     [IsEnabled] BIT NOT NULL,
 
     CONSTRAINT [FK_ApprenticeshipProgressNotification_ApprenticeshipProgress]
-        FOREIGN KEY ([ApprenitceProgressId])
+        FOREIGN KEY ([ApprenticeProgressId])
         REFERENCES [dbo].[ApprenticeshipProgress]([Id]),
 
     CONSTRAINT [FK_ApprenticeshipProgressNotification_ProgressNotification]
